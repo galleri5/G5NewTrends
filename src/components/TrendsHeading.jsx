d4a40239cd605ac6887c27d334134fe6a2d0336c
yaram123 <ramkiran.yaram@galleri5.com>
2025-01-21 10:28:32 +0530

    changing-images-path

--- src/components/TrendsHeading.jsx
+++ src/components/TrendsHeading.jsx
@@ -4,7 +4,7 @@ function TrendsHeading() {
     return (
         <Stack>
             <HStack alignItems={"center"} pt={"24px"} pl={"24px"} >
-                <Image src="./src/assets/galleri5logo.svg" alt="galleri5logo" mt={"10px"} />
+                <Image src="./assets/galleri5logo.svg" alt="galleri5logo" mt={"10px"} />
                 <Text fontWeight={"700"} fontSize={"24px"} color={"#000000"}>Trends</Text>
             </HStack>
             <HStack border={"0.5px solid rgba(0, 0, 0, 0.1)"} width={"100%"} />
--- src/screens/Trends.jsx
+++ src/screens/Trends.jsx
@@ -1,9 +1,40 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { HStack, Image, Stack, Text, VStack } from "@chakra-ui/react"
 import ListOfTrendsTable from "@/components/ListOfTrendsTable"
+import { useEffect, useRef, useState } from "react"
+
+const optionsOfTrendingNow = [
+    {
+        img: "/src/assets/Clockicon.svg",
+        title: "Trending Now",
+    },
+    {
+        img: "/src/assets/Trending up.svg",
+        title: "Emerging Trends"
+    },
+    {
+        img: "/src/assets/Trending down.svg",
+        title: "Declining Trends"
+    }
+]
 function Trends() {
     const { selectedtrend, trending } = useParams()
     const navigate = useNavigate();
+    const [openTrendingNow, setOpenTrendingNow] = useState(false);
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setOpenTrendingNow(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
     console.log(trending)
     return (
         <Stack padding={"20px"}>
@@ -13,10 +44,36 @@ function Trends() {
             </HStack>
             <VStack alignItems={"flex-start"} mt={"20px"} gap={"20px"}>
                 <Text fontWeight={"800"} fontSize={"16px"} color={"rgba(248, 198, 7, 1)"}>{selectedtrend}</Text>
+
                 <VStack alignItems={"flex-start"}>
-                    <HStack>
-                        <Text fontWeight={"700"} fontSize={"18px"} color={"rgba(0, 0, 0, 1)"}>{trending}</Text>
-                        <Image src="/src/assets/dropdownlogo.svg" alt="trend" />
+                    <HStack ref={dropdownRef} onClick={() => setOpenTrendingNow(!openTrendingNow)} position="relative">
+                        <Text fontWeight={"700"} color={"rgba(0, 0, 0, 1)"} fontSize={"18px"}>{trending}</Text>
+                        <Image src="/src/assets/dropdownlogo.svg" alt="Trending Now" />
+
+                        {openTrendingNow && (
+                            <VStack
+                                position="absolute"
+                                top="120%"
+                                left="0"
+                                borderWidth={"1px 3px 3px 1px"}
+                                padding={"12px"}
+                                borderRadius={"8px"}
+                                background={"rgba(255, 255, 255, 1)"}
+                                minW={"230px"}
+                                borderColor={"rgba(0, 0, 0, 1)"}
+                                zIndex={10}
+                                alignItems={"flex-start"}
+                            >
+                                {optionsOfTrendingNow.map((option, index) => (
+                                    <HStack onClick={() => { navigate(`/trends/${selectedtrend}/${option?.title}`) }} key={index} background={option?.title == trending ? "rgba(251, 221, 107, 1)" : ""} padding={"12px"} w={"100%"} borderRadius={"8px"}>
+                                        <Image w={"16px"} h={"16px"} src={option.img} alt={option.title} />
+                                        <Text fontWeight={"600"} color={"rgba(0, 0, 0, 1)"} fontSize={"14px"}>
+                                            {option.title}
+                                        </Text>
+                                    </HStack>
+                                ))}
+                            </VStack>
+                        )}
                     </HStack>
                     <Text fontWeight={"500"} fontSize={"14px"} color={"rgba(0, 0, 0, 1)"}>Discover what’s capturing attention right now in real time.</Text>
                 </VStack>
